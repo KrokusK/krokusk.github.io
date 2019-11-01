586dd9ec2b32772d598d5b4ec9fdef78df479e2b
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-01 19:06:46 +0600

    Changes # 2 01.11.19
    Changes # 2 01.11.19

---
+++ advanced/frontend/models/Companyapp.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Company_description".
+ *
+ * @property string $company_name
+ * @property int $city_id
+ * @property string $company_address
+ */
+class Companyapp extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'company_description';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['company_name', 'city_id', 'company_address', 'company_site', 'count_projects', 'company_staff', 'company_phone', 'company_experience', 'cost_hour', 'company_competence', 'application_id'], 'required'],
+            [['company_name'], 'string', 'max' => 255],
+            [['city_id'], 'default', 'value' => 1],
+            [['city_id'], 'integer'],
+            [['company_address'], 'string', 'max' => 255],
+            [['company_site'], 'string', 'max' => 255],
+            [['count_projects'], 'integer'],
+            [['company_staff'], 'string', 'max' => 255],
+            [['company_phone'], 'integer', 'max' => 10],
+            [['company_experience'], 'string', 'max' => 255],
+            [['cost_hour'], 'float'],
+            [['company_competence'], 'string', 'max' => 255],
+            [['application_id'], 'default', 'value' => 1],
+            [['application_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+ /*   public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
+ */
+}
--- advanced/frontend/views/companyapp/app-addprofile.php
+++ advanced/frontend/views/companyapp/app-addprofile.php
@@ -1,32 +1,53 @@
+<div class="modal-content animated bounceInTop" >
+    <?php
+    $form = ActiveForm::begin(['id' => 'form-add-app-profile', 'enableAjaxValidation' => true, 'validationUrl' => Yii::$app->urlManager->createUrl('companyapp/profilevalidate')]);
+    ?>
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title text-left">Add Contact</h4>
+    </div>
+    <div class="modal-body">
+        <?= $form->field($model, 'company_name')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'company_phone')->textInput(['maxlength' => true]) ?>
+        <div class=" view-btn text-left">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-default' : 'btn btn-default']) ?>
+            <button  type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+        </div>
+    </div>
+    <?php ActiveForm::end(); ?>
+</div>
+
 <?php
-//use yii\helpers\Html;
-use  yii\bootstrap\Modal;
-
-?>
-<h1>Company Application Form</h1>
-
-<p>Test message!</p>
-
-<button type="button" class="btn btn-primary" id="add-app">Подать заявку</button>
-
-<!--
-<span  class="hand-cursor-pointer quick-add-contact" title="Add Contact"><i class="fa fa-plus-circle" aria-hidden="true"></i>Add Contact Via Model</span>
--->
-
-<?php
-$url = Yii::$app->urlManager->createUrl('companyapp/addprofile');
-
 $script = <<< JS
-//QUICK CREARE CONTACT MODEL
-$(document).on('click', '#add-app', function () {       
-    $('#addAppFormModel').modal('show').find('.modal-dialog').load('$url');
-});
+
+   $(document).ready(function () { 
+        $("#form-add-app-profile").on('beforeSubmit', function (event) { 
+            event.preventDefault();            
+            var form_data = new FormData($('#form-add-app-profile')[0]);
+            $.ajax({
+                   url: $("#form-add-app-profile").attr('action'), 
+                   dataType: 'JSON',  
+                   cache: false,
+                   contentType: false,
+                   processData: false,
+                   data: form_data, //$(this).serialize(),                      
+                   type: 'post',                        
+                   beforeSend: function() {
+                   },
+                   success: function(response){                      
+                       toastr.success("",response.message); 
+                       $('#addAppFormModel').modal('hide');
+                   },
+                   complete: function() {
+                   },
+                   error: function (data) {
+                      toastr.warning("","There may a error on uploading. Try again later");    
+                   }
+                });                
+            return false;
+        });
+    });       
 
 JS;
 $this->registerJs($script);
-?>
-
-<!-- POPUP MODAL CONTACT -->                            
-<div class="modal inmodal contact" id="addAppFormModel" role="dialog" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog modal-md "></div>
-</div> 
