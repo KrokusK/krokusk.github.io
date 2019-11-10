dd6d2efb4380d630847e18b9f294a109100e7b34
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-10 18:07:03 +0600

    changes # 3 10.11.19
    changes # 3 10.11.19

--- advanced/frontend/views/site/indexListView.php
+++ advanced/frontend/views/site/indexListView.php
@@ -38,6 +38,19 @@ $this->title = 'Сайт объявлений';
         </div><!-- /.container-fluid -->
     </nav>
 
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="..." alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>...</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <h1>Users</h1>
     <ul>
         <?php foreach ($users as $man): ?>
