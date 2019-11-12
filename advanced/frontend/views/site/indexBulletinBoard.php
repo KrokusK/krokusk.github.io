04f1bb7b16e4903f0ae530da84978b17b3a401c1
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-12 12:43:14 +0600

    next

--- advanced/frontend/views/site/indexBulletinBoard.php
+++ advanced/frontend/views/site/indexBulletinBoard.php
@@ -46,7 +46,7 @@ $this->title = 'Сайт объявлений';
             <?php foreach ($userAds as $userAd): ?>
                 <div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
-                        <img src="" alt="Image">
+                        <img src="<?= Html::encode("{$userAd->adPhotos[0]["photo_path"]}") ?>" alt="Image">
                         <div class="caption">
                             <h3><?= Html::encode("{$userAd->header}") ?></h3>
                             <p>Цена: <?= Html::encode("{$userAd->amount}") ?></p>
