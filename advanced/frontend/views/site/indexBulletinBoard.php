ae84ee6ef18c628e724f35acb4714dd2c00ac3f8
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-12 20:22:09 +0600

    Update indexBulletinBoard.php

--- advanced/frontend/views/site/indexBulletinBoard.php
+++ advanced/frontend/views/site/indexBulletinBoard.php
@@ -20,33 +20,17 @@ $this->title = 'Сайт объявлений';
                     <?php ActiveForm::begin(['id' => 'form-category', 'action' => Yii::$app->urlManager->createUrl('site/index')]); ?>
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav navbar-left">
-                            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                            <li><a href="#">Link</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
+                            <li>
+                                <label class="control-label" for="ad-city">Город</label>
+                                <select id="ad-city" class="form-control" name="ArrayCity[city]">
+                                    <?php echo $selectCity; ?>
+                                </select>
                             </li>
                             <li>
-                                <div class="form-group">
-                                    <label class="control-label" for="ad-city">Город</label>
-                                    <select id="ad-city" class="form-control" name="ArrayCity[city]">
-                                        <?php echo $selectCity; ?>
-                                    </select>
-                            </li>
-                            <li>
-                                    <label class="control-label" for="ad-city">Категория</label>
-                                    <select id="ad-city" class="form-control" name="ArrayCity[city]">
-                                        <?php echo $selectCategory; ?>
-                                    </select>
-                                </div>
+                                <label class="control-label" for="ad-city">Категория</label>
+                                <select id="ad-city" class="form-control" name="ArrayCity[city]">
+                                    <?php echo $selectCategory; ?>
+                                </select>
                             </li>
                             <li>
                                 <?= Html::a(
@@ -55,13 +39,15 @@ $this->title = 'Сайт объявлений';
                                     ['class' => 'btn btn-lg btn-primary']
                                 ) ?>
                             </li>
+                            <li>
+                                <form class="navbar-form navbar-right">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Search">
+                                    </div>
+                                    <button type="submit" class="btn btn-default">Submit</button>
+                                </form>
+                            </li>
                         </ul>
-                        <form class="navbar-form navbar-right">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search">
-                            </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                        </form>
                     </div><!-- /.navbar-collapse -->
                     <?php ActiveForm::end(); ?>
                 </div><!-- /.container-fluid -->
