481204d76f2883a10a24b8938aafaec00ba22f1c
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-15 09:47:20 +0600

    d

--- advanced/frontend/views/site/adById.php
+++ advanced/frontend/views/site/adById.php
@@ -17,19 +17,21 @@ $this->title = 'Сайт объявлений';
 
 
 
-                <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
-                        <img src="<?= Html::encode("{$userAd->adPhotos[0]["photo_path"]}") ?>" alt="Image">
-                        <div class="caption">
-                            <p><?= echo var_dump($userAd); ?></p>
-                            <h3><?= Html::encode("{$idAd}") ?></h3>
-                            <h3><?= Html::encode("{$userAd->header}") ?></h3>
-                            <p>Цена: <?= Html::encode("{$userAd->amount}") ?></p>
-                            <p>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?></p>
-                            <p><a href="#" class="btn btn-primary" role="button">Посмотреть</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-                        </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <?php foreach ($userAd->adPhotos as $objPhoto): ?>
+                        <img src="<?= Html::encode("{$objPhoto["photo_path"]}") ?>" alt="Image">
+                    <?php endforeach; ?>
+                    <div class="caption">
+                        <p><?php var_dump($userAd->adPhotos); ?></p>
+                        <h3><?= Html::encode("{$idAd}") ?></h3>
+                        <h3><?= Html::encode("{$userAd->header}") ?></h3>
+                        <p>Цена: <?= Html::encode("{$userAd->amount}") ?></p>
+                        <p>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?></p>
+                        <p><a href="#" class="btn btn-primary" role="button">Посмотреть</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
                     </div>
                 </div>
+            </div>
 
 
 
