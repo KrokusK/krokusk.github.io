e7fe1985514b486c9991976f5c0ca608129f1669
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-15 13:40:45 +0600

    h

--- advanced/frontend/views/site/adById.php
+++ advanced/frontend/views/site/adById.php
@@ -27,17 +27,18 @@ $this->title = 'Сайт объявлений';
                                 </div>
 
                             <?php endforeach; ?>
-                        
-                    <div class="caption">
-                        <p><?php var_dump($userAd->adPhotos); ?></p>
-                        <h3><?= Html::encode("{$idAd}") ?></h3>
-                        <h3><?= Html::encode("{$userAd->header}") ?></h3>
-                        <p>Цена: <?= Html::encode("{$userAd->amount}") ?></p>
-                        <p>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?></p>
-                        <p><a href="#" class="btn btn-primary" role="button">Посмотреть</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-                    </div>
+
+
                 </div>
 
+            <div class="caption">
+                <p><?php var_dump($userAd->adPhotos); ?></p>
+                <h3><?= Html::encode("{$idAd}") ?></h3>
+                <h3><?= Html::encode("{$userAd->header}") ?></h3>
+                <p>Цена: <?= Html::encode("{$userAd->amount}") ?></p>
+                <p>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?></p>
+                <p><a href="#" class="btn btn-primary" role="button">Посмотреть</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            </div>
 
 
 
