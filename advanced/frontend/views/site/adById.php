8e49af3409eac54e991bd9be719f901fcac388e7
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-15 23:10:22 +0600

    sdsd

--- advanced/frontend/views/site/adById.php
+++ advanced/frontend/views/site/adById.php
@@ -61,13 +61,25 @@ $this->title = 'Сайт объявлений';
         <div class="row">
             <div class="col-md-8">
                 <div class="thumbnail">
-                    <img src="<?= Html::encode("{$userAd->adPhotos[0]["photo_path"]}") ?>" alt="Image">
-                    <div class="caption">
-                        <h3><?= Html::encode("{$userAd->header}") ?></h3>
-                        <p>Цена: <?= Html::encode("{$userAd->amount}") ?></p>
-                        <p>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?></p>
-                        <p><a href="#" class="btn btn-primary" role="button">Посмотреть</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    <div class="content-main">
+                        <?php foreach ($userAd->adPhotos as $objPhoto): ?>
+
+                            <div class="col-sm-6 col-md-4">
+                                <img src="<?= Html::encode("{$objPhoto["photo_path"]}") ?>" alt="Image">
+                            </div>
+
+                        <?php endforeach; ?>
                     </div>
+                    <div class="content-secondary">
+                        <div class="caption">
+                            <h3><?= Html::encode("{$userAd->header}") ?></h3>
+                            <p>Цена: <?= Html::encode("{$userAd->amount}") ?></p>
+                            <p>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?></p>
+                            <p><a href="#" class="btn btn-primary" role="button">Посмотреть</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             <div class="col-md-4">
