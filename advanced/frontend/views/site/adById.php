28e9bf41597474c1b41403e6ceed0ef932d395f8
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-16 13:36:42 +0600

    sd

--- advanced/frontend/views/site/adById.php
+++ advanced/frontend/views/site/adById.php
@@ -20,7 +20,7 @@ $this->title = 'Сайт объявлений';
                     <div class="content-main">
                         <div class="col-sm-6 col-md-9 col-lg-9">
                             <h3><?= Html::encode("{$userAd->header}") ?></h3>
-                            <h4>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?>   Город: <?= Html::encode(date('d.m.Y H:i:s', $userAd->userCities[0]['city_name'])) ?></h4>
+                            <h4>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?>   Город: <?= Html::encode("{$userAd->userCities['city_name']}") ?></h4>
                         </div>
                         <div class="col-sm-6 col-md-3 col-lg-3">
                             <h3></h3>
@@ -43,7 +43,7 @@ $this->title = 'Сайт объявлений';
                             <br><br>
                             <p><?= Html::encode("{$userAd->content}") ?></p>
                             <p>
-                                <p><?php var_dump($userAd->adPhotos); ?></p>
+                                <p><?php var_dump($userAd->userDescs); ?></p>
                             </p>
                         </div>
                     </div>
