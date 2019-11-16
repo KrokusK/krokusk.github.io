a3f0109f4a671269ad19eb16b593f4bad6a98d2e
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-16 10:49:52 +0600

    Update adById.php

--- advanced/frontend/views/site/adById.php
+++ advanced/frontend/views/site/adById.php
@@ -19,11 +19,11 @@ $this->title = 'Сайт объявлений';
                 <div class="thumbnail">
 
                     <div class="content-main">
-                        <div class="col-sm-6 col-md-10 col-lg-10">
+                        <div class="col-sm-6 col-md-9 col-lg-9">
                             <h3><?= Html::encode("{$userAd->header}") ?></h3>
                             <h4>Создано: <?= Html::encode(date('d.m.Y H:i:s', $userAd->created_at)) ?>   Город:</h4>
                         </div>
-                        <div class="col-sm-6 col-md-2 col-lg-10">
+                        <div class="col-sm-6 col-md-3 col-lg-3">
                             <h3></h3>
                             <h4>Цена: <?= Html::encode("{$userAd->amount}") ?><h4>
                         </div>
