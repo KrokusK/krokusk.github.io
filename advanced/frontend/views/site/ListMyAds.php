eb99f05451f715fb1e1a346a076084abd25fe486
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-22 21:25:39 +0600

    h

--- advanced/frontend/views/site/ListMyAds.php
+++ advanced/frontend/views/site/ListMyAds.php
@@ -81,7 +81,7 @@ $this->title = 'Сайт объявлений';
                                     <div class="text-left align-top">
                                         <h4>Статус : <?= Html::encode("{$userAd->adStatus["name"]}") ?></h4>
                                         <h4>Цена: <?= Html::encode("{$userAd->amount}") ?> руб.</h4>
-                                        <img class="text-right align-top" id="AdPhoto<?= Html::encode("{$userAd["id"]}") ?>" src="<?= Html::encode("{$userAd->adPhotos[0]["photo_path"]}") ?>" alt="Image">
+                                        <a><img class="text-right align-top" id="AdPhoto<?= Html::encode("{$userAd["id"]}") ?>" src="<?= Html::encode("{$userAd->adPhotos[0]["photo_path"]}") ?>" alt="Image"></a>
                                     </div>
                             </td>
                         </tr>
