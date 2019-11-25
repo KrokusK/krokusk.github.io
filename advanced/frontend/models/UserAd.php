5d581296d2baa8f18fd50d8f395635d33139e9cd
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-25 12:21:59 +0600

    e

--- advanced/frontend/models/UserAd.php
+++ advanced/frontend/models/UserAd.php
@@ -19,6 +19,49 @@ class UserAd extends \yii\db\ActiveRecord
     }
 
     /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_desc_id', 'status_id', 'header', 'content', 'city_id', 'amount', 'category_id'], 'required', 'message' => 'Поле должно быть заполнено'],
+            [['status_id'], 'in', 'range' =>
+                function ( $attribute, $params ) {
+                    $statusesId = AdStatus::find()->select(['id'])->asArray()->all();
+                    $statusesIdStr = [];
+                    foreach ($statusesId as $item) {
+                        array_push($statusesIdStr, "{$item['id']}");
+                    }
+                    return $statusesIdStr;
+                },
+                'message' => 'Статус не выбран из списка'],
+            [['header'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
+            [['content'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
+            [['city_id'], 'in', 'range' =>
+                function ( $attribute, $params ) {
+                    $citiesId = UserCity::find()->select(['id'])->asArray()->all();
+                    $citiesIdStr = [];
+                    foreach ($citiesId as $item) {
+                        array_push($citiesIdStr, "{$item['id']}");
+                    }
+                    return $citiesIdStr;
+                },
+                'message' => 'Город не выбран из списка'],
+            [['amount'], 'double', 'message' => 'Значение должно быить числом'],
+            [['category_id'], 'in', 'range' =>
+                function ( $attribute, $params ) {
+                    $categoriesId = AdCategory::find()->select(['id'])->asArray()->all();
+                    $categoriesIdStr = [];
+                    foreach ($categoriesId as $item) {
+                        array_push($categoriesIdStr, "{$item['id']}");
+                    }
+                    return $categoriesIdStr;
+                },
+                'message' => 'Категория не выбрана из списка'],
+        ];
+    }
+
+    /**
      *
      * Link to table User_description
      */
