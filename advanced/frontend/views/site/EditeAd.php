dcba08b60ddf8ecbf4a450a7d197e7e2286ded0f
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-26 16:43:49 +0600

    r

--- advanced/frontend/views/site/EditeAd.php
+++ advanced/frontend/views/site/EditeAd.php
@@ -41,8 +41,10 @@ $this->title = 'Сайт объявлений';
 
                                         <div class="thumbnail">
                                             <div class=" view-btn text-center">
-
-                                                <?php echo $form->field($modelPhotoAd, 'imageFiles[]')->fileInput(['class' => 'form-control', 'multiple' => true])->hint('Пожалуйста, загрузите фотографии (не более 3-х фотографий и не более 10 Мб каждая)')->label('Фотографии'); ?>
+                                                <div class="preview">
+                                                    <p>No files currently selected for upload</p>
+                                                </div>
+                                                <?php echo $form->field($modelPhotoAd, 'imageFiles[]')->fileInput(['id' => 'files-send-ad', 'class' => 'form-control', 'multiple' => true])->hint('Пожалуйста, загрузите фотографии (не более 3-х фотографий и не более 10 Мб каждая)')->label('Фотографии'); ?>
                                             </div>
                                         </div>
 
@@ -144,8 +146,81 @@ $script = <<< JS
 
         });
         
+        //update images 
+        var input = document.querySelector('#files-send-ad');
+        var preview = document.querySelector('.preview');
+    
+        input.style.opacity = 0;
+        
+        input.addEventListener('change', updateImageDisplay);
+        
+        function updateImageDisplay() {
+            while(preview.firstChild) {
+                preview.removeChild(preview.firstChild);
+            }
+            
+            var curFiles = input.files;
+            if(curFiles.length === 0) {
+                var para = document.createElement('p');
+                para.textContent = 'No files currently selected for upload';
+                preview.appendChild(para);
+            } else {
+                var list = document.createElement('ol');
+                preview.appendChild(list);
+                for(var i = 0; i < curFiles.length; i++) {
+                    var listItem = document.createElement('li');
+                    var para = document.createElement('p');
+                    if(validFileType(curFiles[i])) {
+                        para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
+                        var image = document.createElement('img');
+                        image.src = window.URL.createObjectURL(curFiles[i]);
+                
+                        listItem.appendChild(image);
+                        listItem.appendChild(para);
+                
+                    } else {
+                        para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
+                        listItem.appendChild(para);
+                    }
+                
+                    list.appendChild(listItem);
+                }
+            }
+        }
+        
+        var fileTypes = [
+            'image/gif',
+            'image/jpeg',
+            'image/pjpeg',
+            'image/png'
+        ]
+        
+        function validFileType(file) {
+              for(var i = 0; i < fileTypes.length; i++) {
+                    if(file.type === fileTypes[i]) {
+                      return true;
+                    }
+              }
+        
+          return false;
+        }
+        
+        function returnFileSize(number) {
+              if(number < 1024) {
+                return number + 'bytes';
+              } else if(number > 1024 && number < 1048576) {
+                return (number/1024).toFixed(1) + 'KB';
+              } else if(number > 1048576) {
+                return (number/1048576).toFixed(1) + 'MB';
+              }
+        }
         
     });       
+
+
+
+
+
 
 JS;
 $this->registerJs($script);
