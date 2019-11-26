e177d586548fcdf975061c8cc4bc26996ab63334
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-26 08:23:07 +0600

    k

--- advanced/frontend/models/PhotoAd.php
+++ advanced/frontend/models/PhotoAd.php
@@ -36,8 +36,8 @@ class PhotoAd extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['ad_id', 'photo_path'], 'required'],
-            [['photo_path'], 'string', 'max' => 255],
+            //[['ad_id', 'photo_path'], 'required'], 
+            //[['photo_path'], 'string', 'max' => 255],
             [['imageFiles'], 'file', 'skipOnEmpty' => false, 'maxFiles' => 3,'mimeTypes' => ['image/gif', 'image/jpeg', 'image/pjpeg', 'image/png'], 'extensions' => ['gif', 'jpg', 'jpeg', 'png'], 'maxSize' => 5*1024*1024, 'message' => 'Файл не соответствует требованиям'],
         ];
     }
