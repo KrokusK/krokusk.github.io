7f6991103fb8193f870f1ed65d84a7633875a896
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-11-28 10:44:06 +0600

    g

--- advanced/frontend/controllers/SiteController.php
+++ advanced/frontend/controllers/SiteController.php
@@ -766,7 +766,7 @@ class SiteController extends Controller
                         }
                     }
 
-                    return Json::encode(array('status' => '1', 'type' => 'success', 'message' => 'Ваше объявление успешно сохранено. modelUserAd->attributes : '.$modelUserAd->attributes.' \n modelUserAdId->attributes : '.$modelUserAdId->attributes));
+                    return Json::encode(array('status' => '1', 'type' => 'success', 'message' => 'Ваше объявление успешно сохранено. modelUserAdId->attributes : '.$modelUserAdId->attributes));
 
                 } else {
                     return Json::encode(array('status' => '0', 'type' => 'warning', 'message' => 'Ваше объявление не может быть сохранено. var5'.var_dump($modelUserAd->user_desc_id, $modelUserAd->status_id, $modelUserAd->created_at, $modelUserAd->updated_at, $modelUserAd->header, $modelUserAd->content, $modelUserAd->city_id, $modelUserAd->amount, $modelUserAd->category_id)));
